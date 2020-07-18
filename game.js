f354ca8a347c0a8ef47c423756502ad7124bbde0
ElaySuperCoder <36122259+Maharramli@users.noreply.github.com>
2020-07-18 12:22:24 +0400

    feat: enemies added

--- game.js
+++ game.js
@@ -3,17 +3,36 @@ ctx = canvas.getContext("2d");
 
 document.addEventListener("keydown", keyPush);
 
+class Helper
+{
+    static getRandomInt(min, max)
+    {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    static removeIndex(array, index)
+    {
+        if (index >= array.length || array.length <= 0)
+        {
+            return;
+        }
+        array[index] = array[array.length - 1];
+        array[array.length - 1] = undefined;
+        array.length = array.length - 1;
+    }
+}
+
 function keyPush()
 {
     if (event.keyCode === 37)
     {
-        ctx.clearRect(0,0,800,700);
         game.ship.x -= game.ship.dx;
     }
 
     if (event.keyCode === 39)
     {
-        ctx.clearRect(0,0,800,700);
         game.ship.x += game.ship.dx;
     }
 }
@@ -45,11 +64,44 @@ class Ship
     }
 }
 
+class Enemy
+{
+    constructor(x, y, dx, dy, enemyDirection, context) {
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+        this.enemyDirection = enemyDirection;
+        this.w = 51;
+        this.h = 40;
+        this.ctx = context;
+        this.img = new Image();
+        this.img.src = 'img/enemy.png';
+    }
+
+    update()
+    {
+    }
+
+    draw()
+    {
+        this.ctx.drawImage(
+            this.img,
+            this.x, this.y,
+            this.w, this.h
+        )
+    }
+}
+
 class Game
 {
     constructor(context) {
         this.ctx = context;
-        this.ship = new Ship(100, 550, 10, this.ctx);
+        this.ship = new Ship(100, 550, 12, this.ctx);
+        this.enemy = new Enemy(2000, 0, Helper.getRandomInt(1,2),Helper.getRandomInt(2,3),Helper.getRandomInt(0,1),this.ctx);
+        this.enemies = [];
+        this.enemyTimer = 0;
+        this.enemySpawnInterval = 50;
         this.loop();
     }
 
@@ -63,11 +115,58 @@ class Game
     update()
     {
         this.ship.update();
+        this.enemy.update();
+
+        if (this.enemyTimer % this.enemySpawnInterval === 0)
+        {
+            this.enemies.push(new Enemy(
+                Helper.getRandomInt(1, 800 - this.enemy.w),
+                -50,
+                Helper.getRandomInt(1,2),
+                Helper.getRandomInt(2,3),
+                Helper.getRandomInt(0,1),
+                this.ctx
+            ));
+
+            this.enemyTimer = 0;
+        }
+
+        this.enemyTimer++;
+
+        this.enemies.forEach((enemy, index) => {
+
+            if (enemy.enemyDirection === 0)
+            {
+                enemy.x -= enemy.dx;
+                enemy.y += enemy.dy;
+            }
+            if (enemy.enemyDirection === 1)
+            {
+                enemy.x += enemy.dx;
+                enemy.y += enemy.dy;
+            }
+
+            if (enemy.y > 700)
+            {
+                Helper.removeIndex(this.enemies, index);
+            }
+
+            enemy.update();
+        });
     }
 
     draw()
     {
+        ctx.clearRect(0,0,800,700);
         this.ship.draw();
+        this.enemy.draw();
+        for (let i in this.enemies)
+        {
+            if (this.enemies.hasOwnProperty(i))
+            {
+                this.enemies[i].draw();
+            }
+        }
     }
 }
 
